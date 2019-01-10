1e925ca7d30ef92c4b1e469e7c0b022359b27b3e
Kgupi <Kgupi@users.noreply.github.com>
2019-01-10 22:52:44 +0200

    Added VDNStd project to the newly created VDNStd solution. Added StringCharCheck and VDEventLog utility classes to the newly created VDNStd project.

---
+++ VDNStd/General/StringCharCheck.cs
@@ -0,0 +1,35 @@
+﻿#region Proprietary Information
+/* 
+    * Created by:   Vision-Dream ICT Solutions
+    * Author:       Onkgopotse Lenake
+    * Email:        visiondreamict@gmail.com
+    * Website:      www.visiondreamict.wordpress.com
+    * 
+    * Copyright (c) 2019 Vision-Dream ICT Solutions. All rights reserved.
+    * ___________________________________________________________________
+    * Project:      Vision-Dream .Net Standard 2.0 (VDNStd) 
+    *               Project Targeting .Net Standard 2.0.
+    * Version:      v1.0.0
+    * File:         StringCharCheck.cs
+    * Date:         2019-01-10
+    * Description:  This file contains the StringCharCheck class. 
+    *               Class execution code.
+*/
+#endregion
+
+using System;
+
+namespace VDNStd.General
+{
+    public class StringCharCheck
+    {
+        public static bool IsFirstUpper(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
---
+++ VDNStd/General/VDEventLog.cs
@@ -0,0 +1,146 @@
+﻿#region Proprietary Information
+/* 
+    * Created by:   Vision-Dream ICT Solutions
+    * Author:       Onkgopotse Lenake
+    * Email:        visiondreamict@gmail.com
+    * Website:      www.visiondreamict.wordpress.com
+    * 
+    * Copyright (c) 2019 Vision-Dream ICT Solutions. All rights reserved.
+    * ___________________________________________________________________
+    * Project:      Vision-Dream .Net Standard 2.0 (VDNStd) 
+    *               Project Targeting .Net Standard 2.0.
+    * Version:      v1.0.0
+    * File:         VDEventLog.cs
+    * Date:         2019-01-10
+    * Description:  This file contains the VDEventLog class. 
+    *               Class execution code.
+*/
+#endregion
+
+using System;
+using System.IO;
+
+namespace VDNStd.General
+{
+    /// <summary>
+    /// Custom Logging Class.
+    /// </summary>
+    /// <value>
+    /// Utility for writing custom logs to a file(s) using <see cref="VDEventLog"/>.
+    /// Returning formated output data to be used.
+    /// </value>
+    /// <remarks>
+    /// <para>
+    /// Writes formatted string data to a specified log file(s).
+    /// Returns formatted output string data to be streamed or written to standard output.
+    /// </para>
+    /// </remarks>
+    public class VDEventLog
+    {
+        #region Fields
+        /// <summary> Input
+        /// </summary>
+        private static string _LogFile;
+        /// <summary> Internal
+        /// </summary>
+        private static string _DateTime;
+        #endregion
+
+        #region Constructors
+        /// <summary> Default Constructor
+        /// </summary>
+        public VDEventLog() { }
+        /// <summary> File name parameter constructor
+        /// </summary>
+        public VDEventLog(string logfile)
+        {
+            _LogFile = logfile;
+        }
+        #endregion
+
+        #region Public Static Methods
+        /// <summary>
+        /// <see cref="ReturnLog"/> Method.
+        /// </summary>
+        /// <value>
+        /// Return a formatted string to write.
+        /// </value>
+        public static string ReturnLog(string process)
+        {
+            _DateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            return string.Format("{0} {1}", _DateTime, process);
+        }
+
+        public static string ReturnLog(string process, string message)
+        {
+            _DateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            return string.Format("{0} [{1}] - {2}", _DateTime, process, message);
+        }
+
+        /// <summary>
+        /// <see cref="WriteLog"/> Method.
+        /// </summary>
+        /// <value>
+        /// FileStream processing event log.
+        /// Write an empty line to a log file.
+        /// </value>
+        public static void WriteLog(string logfile = null)
+        {
+            if (!string.IsNullOrEmpty(logfile))
+                _LogFile = logfile;
+
+            using (FileStream stream = File.Open(_LogFile, FileMode.Append))
+            {
+                using (TextWriter writer = new StreamWriter(stream))
+                    writer.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// <see cref="WriteLog"/> Method.
+        /// </summary>
+        /// <value>
+        /// FileStream processing event log overloading method.
+        /// Write log data to a log file.
+        /// </value>
+        public static void WriteLog(string message, string logfile = null)
+        {
+            if (!string.IsNullOrEmpty(logfile))
+                _LogFile = logfile;
+
+            _DateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            using (FileStream stream = File.Open(_LogFile, FileMode.Append))
+            {
+                using (TextWriter writer = new StreamWriter(stream))
+                    writer.WriteLine("{0} {1}", _DateTime, message);
+            }
+        }
+
+        public static void WriteLog(string process, string message, string logfile = null)
+        {
+            if (!string.IsNullOrEmpty(logfile))
+                _LogFile = logfile;
+
+            _DateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            using (FileStream stream = File.Open(_LogFile, FileMode.Append))
+            {
+                using (TextWriter writer = new StreamWriter(stream))
+                    writer.WriteLine("{0} [{1}] - {2}", _DateTime, process, message);
+            }
+        }
+
+        public static void WriteLog(string process, string message, string status, string logfile = null)
+        {
+            if (!string.IsNullOrEmpty(logfile))
+                _LogFile = logfile;
+
+            _DateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            using (FileStream stream = File.Open(_LogFile, FileMode.Append))
+            {
+                using (TextWriter writer = new StreamWriter(stream))
+                    writer.WriteLine("{0} [{1}] - {2} | Status: {3}", _DateTime, process, message, status);
+            }
+        }
+        #endregion
+    }
+}
