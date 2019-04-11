2443b0d458cae509e36556d478fd28d2fb5ef2c3
Kgupi <Kgupi@users.noreply.github.com>
2019-04-11 07:46:15 +0200

    Enhanced forms for validation in eHomeAffairs and BankSystem projects.

--- HomeAffairs/ClientApp/src/app/register/register-account/register-account.component.ts
+++ HomeAffairs/ClientApp/src/app/register/register-account/register-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-account',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-account.component.css']
 })
 export class RegisterAccountComponent implements OnInit {
+  accPassword = '123456';
+  accConfirmPassword = '123456';
+
+  users = { userID: 1, userEmail: 'gupilenake@gmail.com', userPassword: '' };
+
+  usersProfile = {
+    usersProfileID: 1, userID: 1, email: '',
+    firstName: 'Onkgopotse', lastName: 'Lenake'
+  };
+
+  usersAccountForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    //this.questionsForm = new FormGroup({
+    //  'name': new FormControl(this.hero.name, [
+    //    Validators.required,
+    //    Validators.minLength(4),
+    //    forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom validator.
+    //  ]),
+    //  'alterEgo': new FormControl(this.hero.alterEgo),
+    //  'power': new FormControl(this.hero.power, Validators.required)
+    //});
   }
 
 }
--- HomeAffairs/ClientApp/src/app/register/register-profile/register-profile.component.ts
+++ HomeAffairs/ClientApp/src/app/register/register-profile/register-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-profile',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-profile.component.css']
 })
 export class RegisterProfileComponent implements OnInit {
+  selectedTitle = '--Select Title--';
+  selectedProvince = '*----Select Province----';
+
+  titles = [this.selectedTitle, 'Dr', 'Mr', 'Miss', 'Mrs'];
+
+  provinces = [
+    this.selectedProvince, 'Eastern Cape', 'Free State', 'Gauteng', 'KwaZulu-Natal',
+    'Limpopo', 'Mpumalanga', 'North West', 'Northern Cape', 'Western Cape'
+  ];
+
+  usersProfile = {
+    usersProfileID: 1, userID: 1, title: this.titles[0], initials: '', firstName: 'Onkgopotse',
+    lastName: 'Lenake', idNumber: '', dateOfBirth: '', email: '', cellPhone: '',
+    province: this.provinces[0], address: '', city: '', suburb: '', code: ''
+  };
+
+  usersProfileForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    //this.questionsForm = new FormGroup({
+    //  'name': new FormControl(this.hero.name, [
+    //    Validators.required,
+    //    Validators.minLength(4),
+    //    forbiddenNameValidator(/bob/i) // <-- Here's how you pass in the custom validator.
+    //  ]),
+    //  'alterEgo': new FormControl(this.hero.alterEgo),
+    //  'power': new FormControl(this.hero.power, Validators.required)
+    //});
   }
 
 }
